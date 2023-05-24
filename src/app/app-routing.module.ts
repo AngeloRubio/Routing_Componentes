0e76de97672ffeabb8b9c5182c7fa993e40a8b26
Angelo <angelo.rubiomer@ug.edu.ec>
2023-05-23 21:43:59 -0500

    Se agregaron los componentes de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { EliminarComponent } from './components/eliminar/eliminar.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { ModificarComponent } from './components/modificar/modificar.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'buscar',
+    component: BuscarComponent,
+  },
+  {
+    path: 'eliminar',
+    component: EliminarComponent,
+  },
+  {
+    path: 'lista',
+    component: ListaComponent,
+  },
+  {
+    path: 'modificar',
+    component: ModificarComponent,
+  },
+  {
+    path: 'registrar',
+    component: RegistrarComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { EliminarComponent } from './components/eliminar/eliminar.component';
+import { ModificarComponent } from './components/modificar/modificar.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BuscarComponent,
+    ListaComponent,
+    EliminarComponent,
+    ModificarComponent,
+    RegistrarComponent
   ],
   imports: [
     BrowserModule,
