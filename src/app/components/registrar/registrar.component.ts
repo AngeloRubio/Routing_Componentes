3295ec109696c66c956ebc742ad2337ef59250fc
Angelo <angelo.rubiomer@ug.edu.ec>
2023-05-23 22:40:58 -0500

    componente registrar

--- src/app/components/registrar/registrar.component.ts
+++ src/app/components/registrar/registrar.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrls: ['./registrar.component.css']
+  styleUrls: ['./registrar.component.css'],
 })
 export class RegistrarComponent {
+  nombre: string = '';
+  apellido = '';
+  edad = '';
+  cedula = '';
 
+  registrar() {
+    console.log('Datos registrados:', {
+      nombre: this.nombre,
+      apellido: this.apellido,
+    });
+  }
 }
